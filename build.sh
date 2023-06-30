8007311ab5eefb15e16b9ae0cc2e845e9f6fc6b6
Saigusa Yasushi <56156402+yassi-github@users.noreply.github.com>
2023-07-01 07:34:41 +0900

    replace compile script from makefile to build.sh
    makefile rule is too complex and less scalability
    so to automatically add compile target, shellscript is good way.

---
+++ build.sh
@@ -0,0 +1,172 @@
+#!/bin/bash
+# build.sh
+
+# check compile needs of received source file
+# return true or false
+is_need_compile() {
+    local source_file="${1:?"source file not found"}"
+    local object_file="${source_file/'.c'/'.o'}"
+    local retval=false
+
+    # need compile if object file not found or object file is older than source file
+    if [[ ! -e "${object_file}" ]] || [[ $(date +"%s" -r "${object_file}") < $(date +"%s" -r "${source_file}") ]]; then
+        retval=true
+    fi
+
+    echo ${retval};
+}
+
+
+# build one source
+# build stale libs
+# return 0 if built
+# return 1 if no need to build
+unit_build() {
+    local MAIN="${1:?"build source undefined"}"
+    local SRCS="${MAIN} $(find ${g_LIBDIR}/ -type f -name *.c) "
+    local OBJS="${SRCS//'.c '/'.o '}"
+    local _TARGET="${g_BINDIR}/${MAIN##*/}"
+    local TARGET="${_TARGET%.c}"
+    local CC=gcc
+    local OPTIMIZATION=-O3
+    local DEBUGOPT=""
+    if [[ ${DEBUG} == 1 ]]; then
+        OPTIMIZATION=-O0
+        DEBUGOPT=-g
+    fi
+    local CFLAGS="${DEBUGOPT} -std=c11 -D_POSIX_C_SOURCE=200809L -fstack-protector -I ${g_HEADERDIR} ${OPTIMIZATION} -Wall -Wextra -Werror"
+
+    # ensure dirs exists
+    mkdir -p "${g_HEADERDIR}" "${g_SOURCEDIR}" "${g_LIBDIR}" "${g_BINDIR}"
+
+    # compile object files in parallel
+    echo "${SRCS// /$'\n'}" | xargs -i{} -P$(cat /proc/cpuinfo | grep processor | tail -n1 | grep -o [0-9]*) \
+        bash -c 'source build.sh # read "is_need_compile" function
+                if $(is_need_compile {}); then
+                    echo '"${CC}"' '"${CFLAGS}"' -o $(sed "s/^\(.*\).c$/\1.o/" <<< "{}") -c {}
+                         '"${CC}"' '"${CFLAGS}"' -o $(sed "s/^\(.*\).c$/\1.o/" <<< "{}") -c {}
+                    exit 1
+                fi
+                exit 0'
+    # rc is 123 if there was compile at least once. else 0.
+    local xargs_rc=$?
+
+    # create binary
+    # Now, object file is created so obj is later than src.
+    # do nothing if destination binary already created and there was no obj compile.
+    if [[ -e "${TARGET}" ]] && [[ ${xargs_rc} == 0 ]]; then
+        return 1
+    fi
+    echo ${CC} ${CFLAGS} -o ${TARGET} ${SRCS}
+         ${CC} ${CFLAGS} -o ${TARGET} ${SRCS}
+    return 0
+}
+
+subcmd_test() {
+    local CC=g++
+    local TESTS="$(find ${g_SOURCEDIR}/ -type f -name *_test.cc)"
+
+    # compile in parallel
+    echo "${TESTS// /$'\n'}" | xargs -i{} -P$(cat /proc/cpuinfo | grep processor | tail -n1 | grep -o [0-9]*) \
+        bash -c 'echo [WIP] do_test_compile like '"${CC}"' {}'
+
+}
+
+subcmd_all() {
+    local MAIN_SRCS="$(find ${g_SOURCEDIR}/ -maxdepth 1 -type f -name *.c)"
+
+    echo "${MAIN_SRCS// /$'\n'}" | xargs -i{} -P$(cat /proc/cpuinfo | grep processor | tail -n1 | grep -o [0-9]*) \
+        bash -c 'source ./build.sh
+                unit_build {} ; unit_build_rc=$?
+                [[ ${unit_build_rc} == 0 ]] && exit 1 || exit 0'
+    # xargs tells exitcode 123 when there was non zero exit in the xargs scrpt, after all xargs script done.
+    local xargs_rc=$?
+
+    # xargs_rc 0 means there all failed to compile.
+    # xargs_rc 123 means there was succees to compile at least once.
+    [[ ${xargs_rc} == 0 ]] && echo "Nothing to build."
+}
+
+subcmd_clean() {
+    echo rm -rf $(find -type f -name "*.o") ${g_BINDIR}
+         rm -rf $(find -type f -name "*.o") ${g_BINDIR}
+}
+
+
+main() {    
+    local subcmd="${1:-all}"
+    case "${subcmd}" in
+        "all")
+            subcmd_all
+            exit 0
+        ;;
+        "test")
+            subcmd_test
+            exit 0
+        ;;
+        "clean")
+            subcmd_clean
+            exit 0
+        ;;
+        "help")
+            echo "Usage:"
+            echo -e "\tbuild.sh [option] [subcmd/target_source]"
+            echo -e ""
+            echo -e "\toption:"
+            echo -e "\t\t-d\tEnable debug mode"
+            echo -e ""
+            echo -e "\tsubcmd:"
+            echo -e "\t\tall\tBuild all (default)"
+            echo -e "\t\ttest\tRun test"
+            echo -e "\t\tclean\tRemove bin and obj files"
+            echo -e "\t\thelp\tShow this text"
+            echo -e "\t\t\`target_source\`\tPath to source file to build"
+            echo ""
+            echo "Note: Do nothing if called by any script excepting shell."
+            exit 0
+        ;;
+        *)
+            local target_src="${subcmd}"
+            if [[ -e ${target_src} ]]; then
+                unit_build "${target_src}"
+                if [[ $? == 1 ]]; then
+                    echo "Nothing to build."
+                fi
+                exit 0
+            else
+                echo "build source not found."
+                echo "To show command help, run \"build.sh help\"."
+                exit 1
+            fi
+        ;;
+    esac
+}
+
+
+readonly g_HEADERDIR=include
+readonly g_SOURCEDIR=src
+# readonly g_LIBDIR=${g_SOURCEDIR}/lib
+readonly g_LIBDIR=lib
+readonly g_BINDIR=bin
+# readonly g_TESTDIR=test
+
+DEBUG=0
+while getopts d OPT
+do
+case "${OPT}" in
+    d)
+        echo "build with debug mode."
+        echo ""
+        DEBUG=1
+        shift
+    ;;
+esac
+done
+
+# soruce guard
+# run main when called by shell
+if [[ ${0##*/} == "build.sh" ]]; then
+    main "${1}"
+else
+    :
+fi
