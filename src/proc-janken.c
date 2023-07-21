0a7041d5cd9ef9cb5a24c5cac04013380b206455
Saigusa Yasushi <56156402+yassi-github@users.noreply.github.com>
2023-07-22 05:03:11 +0900

    proc-janken: impl

--- src/proc-janken.c
+++ src/proc-janken.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>  //exit
+#include <string.h>  // strcmp
 #include <unistd.h>  //fork
 
 #include "errorutil.h"
+#include "qutil.h"
 #include "randomutil.h"
-
-#define _DEFAULT_SOURCE 1
+#include "waitutil.h"
 
 // internal error messages
 struct _error_msg_list {
@@ -20,20 +22,32 @@ static const struct _error_msg_list error_msg_list = {
 
 typedef enum {
   // ぐー
-  Lock,
+  Rock,
   // ちょき
   Paper,
   // ぱー
   Scissors,
 } JankenCard;
+struct _janken_card_str {
+  const char *rock;
+  const char *paper;
+  const char *scissors;
+  const char *unexpected;
+};
+static const struct _janken_card_str janken_card_str = {
+    .rock = u8"✊",
+    .paper = u8"🖐️",
+    .scissors = u8"✌️",
+    .unexpected = u8"👻",
+};
 
 // child
 
 // じゃんけんの手を決める
-error rand_card(int seed, JankenCard *card) {
+error rand_card(JankenCard *card) {
   error err;
   int random_int;
-  err = randint(seed, &random_int);
+  err = randint(&random_int);
   if (err != NULL) {
     return err;
   }
@@ -41,25 +55,47 @@ error rand_card(int seed, JankenCard *card) {
   return NULL;
 }
 
-error recv_parent_command() { return NULL; }
+// wait till command recv
+error recv_parent_command(int commandq_id) {
+  char _unused;
+  // fixme: dont consume for mul children
+  error err = q_pop(commandq_id, &_unused);
+  if (err != NULL) {
+    return err;
+  }
+  return NULL;
+}
 
-error send_card_to_parent() { return NULL; }
+error send_card_to_parent(int q_id, JankenCard *card) {
+  error err;
+  err = q_push(q_id, (char *)card);
+  if (err != NULL) {
+    return err;
+  }
+  return NULL;
+}
 
-error child_janken_loop(int radom_seed) {
-  // TODO: use msgQ
+error child_janken_loop(int q_id, int commandq_id) {
   error err;
   bool is_loop = true;
   JankenCard card;
+  // (仮: 5回ループ)
+  int _loop_counter = 0;
   while (is_loop) {
-    err = recv_parent_command();
+    _loop_counter += 1;
+    if (_loop_counter > 5) {
+      return NULL;
+    }
+
+    err = recv_parent_command(commandq_id);
     if (err != NULL) {
       return err;
     }
-    err = rand_card(radom_seed, &card);
+    err = rand_card(&card);
     if (err != NULL) {
       return err;
     }
-    err = send_card_to_parent();
+    err = send_card_to_parent(q_id, &card);
     if (err != NULL) {
       return err;
     }
@@ -67,16 +103,15 @@ error child_janken_loop(int radom_seed) {
   return NULL;
 }
 
-error child_process() {
+error child_process(int q_id, int commandq_id) {
   error err;
   // 手を決める乱数を初期化
-  unsigned int random_seed;
-  err = init_seed(&random_seed);
+  err = init_seed();
   if (err != NULL) {
     return err;
   }
   // loop
-  err = child_janken_loop(random_seed);
+  err = child_janken_loop(q_id, commandq_id);
   if (err != NULL) {
     return err;
   }
@@ -85,32 +120,48 @@ error child_process() {
 
 // parent
 
-error init() {
-  // error err;
-  // TODO: init msgQ
+error init(int *q_id, int *commandq_id) {
+  error err;
+  err = q_new(q_id);
+  if (err != NULL) {
+    return err;
+  }
+  err = q_new(commandq_id);
+  if (err != NULL) {
+    return err;
+  }
   return NULL;
 }
 
-error spawn_child(int *child_counter) {
+error spawn_child(int q_id, int commandq_id, int *child_counter) {
   int pid = fork();
   if (pid == -1) {
     return error_new(error_msg_list.error_fork);
   }
-  child_counter += 1;
+  *child_counter += 1;
   // parent process start
   if (pid > 0) {
     return NULL;
   }
   // parent process end
+
   // child process start
-  return child_process();
+  error err = child_process(q_id, commandq_id);
+  if (err != NULL) {
+    error_exit(err);
+  }
+  exit(EXIT_SUCCESS);
   // child process end
 }
 
 // じゃんけんの手入力うけつけ
 error input_card(JankenCard *card) {
   int card_in;
-  printf("input your hand[0,1,2]:");
+  printf(
+      u8"じゃんけん…\n"
+      u8"(%s: %d, %s: %d, %s: %d): ",
+      janken_card_str.rock, Rock, janken_card_str.paper, Paper,
+      janken_card_str.scissors, Scissors);
   if (scanf("%d", &card_in) == EOF) {
     return error_msg_list.error_input;
   }
@@ -121,38 +172,130 @@ error input_card(JankenCard *card) {
   return error_msg_list.error_input;
 }
 
-error send_janken_card_determine_command_to_child() { return NULL; }
-
-error recv_janken_card() { return NULL; }
-
-// 結果表示
-error show_card(JankenCard *child_cards, int child_counter) {
-  for (int idx = 0; idx > child_counter; idx++) {
-    printf("hoi: %d\n", child_cards[idx]);
+error send_janken_card_determine_command_to_child(int commandq_id) {
+  char _unused = 0;
+  error err = q_push(commandq_id, &_unused);
+  if (err != NULL) {
+    return err;
   }
   return NULL;
 }
 
-error parent_janken_loop(int *child_counter) {
-  // TODO: use msgQ
+error recv_janken_card(int q_id, JankenCard *child_card) {
+  char data;
+  error err = q_pop(q_id, &data);
+  if (err != NULL) {
+    return err;
+  }
+  *child_card = (JankenCard)data;
+  return NULL;
+}
+
+const char *janken_result(JankenCard *user_card, JankenCard *system_card) {
+  const char *draw_string = u8"あいこ！";
+  const char *win_string = u8"勝ち！";
+  const char *lose_string = u8"負け！";
+  switch (*user_card) {
+    case Rock:
+      switch (*system_card) {
+        case Rock:
+          return draw_string;
+        case Paper:
+          return lose_string;
+        case Scissors:
+          return win_string;
+      }
+      break;
+    case Paper:
+      switch (*system_card) {
+        case Rock:
+          return win_string;
+        case Paper:
+          return draw_string;
+        case Scissors:
+          return lose_string;
+      }
+      break;
+    case Scissors:
+      switch (*system_card) {
+        case Rock:
+          return lose_string;
+        case Paper:
+          return win_string;
+        case Scissors:
+          return draw_string;
+      }
+      break;
+  }
+  return janken_card_str.unexpected;
+}
+
+const char *janken_tostr(JankenCard *card) {
+  switch (*card) {
+    case Rock:
+      return janken_card_str.rock;
+    case Paper:
+      return janken_card_str.paper;
+    case Scissors:
+      return janken_card_str.scissors;
+  }
+  // unexpected
+  return janken_card_str.unexpected;
+}
+
+// 結果表示
+error show_card(JankenCard *user_card, JankenCard child_cards[],
+                int child_counter) {
+  for (int idx = 0; idx < child_counter; idx++) {
+    printf(
+        u8"ほい\n"
+        u8"あなた: %s vs %s ...%s\n",
+        janken_tostr(user_card), janken_tostr(&(child_cards[idx])),
+        janken_result(user_card, &(child_cards[idx])));
+  }
+  return NULL;
+}
+
+error parent_janken_loop(int q_id, int commandq_id, int child_counter) {
   error err;
   JankenCard user_card;
-  err = input_card(&user_card);
-  if (err != NULL) {
-    return err;
+  bool is_loop = true;
+  // (仮: 5回ループ)
+  int _loop_counter = 0;
+  while (is_loop) {
+    _loop_counter += 1;
+    if (_loop_counter > 5) {
+      return NULL;
+    }
+
+    err = input_card(&user_card);
+    if (err != NULL) {
+      return err;
+    }
+    err = send_janken_card_determine_command_to_child(commandq_id);
+    if (err != NULL) {
+      return err;
+    }
+    JankenCard child_cards[child_counter];
+    for (int child_idx = 0; child_idx < child_counter; child_idx++) {
+      err = recv_janken_card(q_id, &(child_cards[child_idx]));
+      if (err != NULL) {
+        return err;
+      }
+    }
+    err = show_card(&user_card, child_cards, child_counter);
+    if (err != NULL) {
+      return err;
+    }
   }
-  err = send_janken_card_determine_command_to_child();
-  if (err != NULL) {
-    return err;
-  }
-  JankenCard child_card[*child_counter];
-  err = recv_janken_card(child_card, *child_counter);
-  if (err != NULL) {
-    return err;
-  }
-  err = show_card(child_card, *child_counter);
-  if (err != NULL) {
-    return err;
+  return NULL;
+}
+
+// continue waiting until all child processes exited.
+error wait_terminate_all_childlen(int child_process_counter) {
+  while (child_process_counter > 0) {
+    /* wait terminating child process */
+    if (wait_exit() > 0) child_process_counter--;
   }
   return NULL;
 }
@@ -160,25 +303,30 @@ error parent_janken_loop(int *child_counter) {
 // main
 
 int main() {
-  error err;
+  printf("5回じゃんけん\n");
 
-  err = init();
+  error err;
+  int q_id;
+  int commandq_id;
+  err = init(&q_id, &commandq_id);
   if (err != NULL) {
     error_exit(err);
   }
 
   int child_counter = 0;
-  err = spawn_child(&child_counter);
+  err = spawn_child(q_id, commandq_id, &child_counter);
   if (err != NULL) {
     error_exit(err);
   }
 
-  err = parent_janken_loop(&child_counter);
+  err = parent_janken_loop(q_id, commandq_id, child_counter);
   if (err != NULL) {
     error_exit(err);
   }
 
+  err = wait_terminate_all_childlen(child_counter);
+  if (err != NULL) {
+    error_exit(err);
+  }
   return 0;
 }
-
-#undef _DEFAULT_SOURCE
